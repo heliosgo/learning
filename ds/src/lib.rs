480f2b821725dc80ab412a474d592e253a1c1b07
xiejie <imxiejie@gmail.com>
2023-02-22 20:51:14 +0800

    select_sort to selection_sort

--- ds/src/lib.rs
+++ ds/src/lib.rs
@@ -1,5 +1,5 @@
 mod bubble_sort;
 mod queue;
 mod quick_sort;
-mod select_sort;
+mod selection_sort;
 mod stack;
--- ds/src/selection_sort.rs
+++ ds/src/selection_sort.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-fn select_sort(nums: &mut Vec<i32>) {
+fn selection_sort(nums: &mut Vec<i32>) {
     let n = nums.len();
     for i in 0..n {
         let mut min_index = i;
@@ -18,13 +18,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_select_sort() {
+    fn test_selection_sort() {
         let mut nums = vec![4, 5, 2, 1, 3, 6];
-        select_sort(&mut nums);
+        selection_sort(&mut nums);
         assert_eq!(nums, vec![1, 2, 3, 4, 5, 6]);
 
         let mut nums2 = vec![1, 1, 1, 1, 1, 1];
-        select_sort(&mut nums2);
+        selection_sort(&mut nums2);
         assert_eq!(nums2, vec![1, 1, 1, 1, 1, 1]);
     }
 }

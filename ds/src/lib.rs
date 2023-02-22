3d070ab8b4d73e168b2f9ee81186e402d2010a55
xiejie <imxiejie@gmail.com>
2023-02-22 20:44:26 +0800

    add selection sort

--- ds/src/lib.rs
+++ ds/src/lib.rs
@@ -1,3 +1,4 @@
 mod queue;
 mod quick_sort;
+mod select_sort;
 mod stack;
---
+++ ds/src/select_sort.rs
@@ -0,0 +1,30 @@
+#![allow(unused)]
+
+fn select_sort(nums: &mut Vec<i32>) {
+    let n = nums.len();
+    for i in 0..n {
+        let mut min_index = i;
+        for j in i + 1..n {
+            if nums[j] < nums[min_index] {
+                min_index = j;
+            }
+        }
+        nums.swap(min_index, i);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_select_sort() {
+        let mut nums = vec![4, 5, 2, 1, 3, 6];
+        select_sort(&mut nums);
+        assert_eq!(nums, vec![1, 2, 3, 4, 5, 6]);
+
+        let mut nums2 = vec![1, 1, 1, 1, 1, 1];
+        select_sort(&mut nums2);
+        assert_eq!(nums2, vec![1, 1, 1, 1, 1, 1]);
+    }
+}
